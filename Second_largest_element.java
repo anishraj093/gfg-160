c52e617b2ec628613c09be708b7ba98a4ce9e90e
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2024-11-24 10:33:31 +0530

    Create Second_largest_element.java
    24-11-2024

---
+++ Second_largest_element.java
@@ -0,0 +1,18 @@
+//Second Largest in arr
+
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        //find the max element
+        int max=0;
+        for(int e:arr){
+            if(e>max)max=e;
+        }
+        int secondMax=-1;
+        for(int e:arr){
+            if(e>secondMax&&e!=max)secondMax=e;
+        }
+        return secondMax;
+    }
+}
