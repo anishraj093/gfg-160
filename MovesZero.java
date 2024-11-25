7b7c2553e5c308d3672862102ca34beeaf714429
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2024-11-25 23:49:28 +0530

    Create MovesZero.java
    25-11-2024

---
+++ MovesZero.java
@@ -0,0 +1,15 @@
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int n=arr.length;
+        // fill the first i_th index with non-zero element
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]>0)arr[i++]=arr[j];
+        }
+        //fill the remaning position with zero
+        while(i<n){
+            arr[i++]=0;
+        }
+    }
+}
