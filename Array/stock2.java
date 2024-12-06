18e4e62f0abf589a6bdf365d55668797775f4a91
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2024-12-07 00:21:28 +0530

    Update stock2.java
    07-12-2024

--- Array/stock2.java
+++ Array/stock2.java
@@ -13,3 +13,21 @@ class Solution {
         return profit;
     }
 }
+
+
+//buy and sell stock at max 1 times
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // Code here
+        int n=prices.length;
+        int mxprofit=0;
+        int buying_price=prices[0];
+        for(int i=1;i<n;i++){
+            if(buying_price<prices[i]){
+                int profit=prices[i]-buying_price;
+                mxprofit=Math.max(mxprofit,profit);
+            }else buying_price=prices[i];
+        }
+        return mxprofit;
+    }
+}
