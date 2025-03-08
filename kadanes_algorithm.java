4a8d75f12fbc581dbdd30663ad59db1c979a7337
anishraj093 <131684006+anishraj093@users.noreply.github.com>
2025-03-08 10:18:36 +0530

    Create kadanes_algorithm.java

---
+++ kadanes_algorithm.java
@@ -0,0 +1,16 @@
+public class kadanes_algorithm {
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        int currSum=0;
+        int maxSum=0;
+        int max=Integer.MIN_VALUE;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            currSum=Math.max(currSum+arr[i],0);
+            maxSum=Math.max(maxSum,currSum);
+            max=Math.max(max,arr[i]);
+        }
+        if(maxSum==0)return max;
+        return maxSum;
+    }
+}
